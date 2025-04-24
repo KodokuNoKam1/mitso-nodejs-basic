a8eb439bfc7826370f27a8357bb49276edafabaa
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2025-04-24 12:06:40 +0300

    Zlib (src/zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const inputFilePath = './files/fileToCompress.txt';
+    const outputFilePath = './files/archive.gz';
+    const inputFileStream = createReadStream(inputFilePath);
+    const gzipStream = createGzip();
+    const outputFileStream = createWriteStream(outputFilePath);
+    inputFileStream.pipe(gzipStream).pipe(outputFileStream);
+
+    outputFileStream.on('finish', () => {
+        console.log('Файл был успешно сжат в archive.gz');
+    });
+
+    outputFileStream.on('error', (err) => {
+        console.error('Ошибка при записи сжатого файла:', err);
+    });
+
+    inputFileStream.on('error', (err) => {
+        console.error('Ошибка при чтении исходного файла:', err);
+    }); 
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFilePath = './files/archive.gz';
+    const outputFilePath = './files/fileToCompress.txt';
+    const inputFileStream = createReadStream(inputFilePath);
+    const gunzipStream = createGunzip();
+    const outputFileStream = createWriteStream(outputFilePath);
+    inputFileStream.pipe(gunzipStream).pipe(outputFileStream);
+
+    outputFileStream.on('finish', () => {
+        console.log('Файл был успешно распакован в fileToCompress.txt');
+    });
+
+    outputFileStream.on('error', (err) => {
+        console.error('Ошибка при записи распакованного файла:', err);
+    });
+
+    inputFileStream.on('error', (err) => {
+        console.error('Ошибка при чтении сжатого файла:', err);
+    }); 
 };
 
 await decompress();
