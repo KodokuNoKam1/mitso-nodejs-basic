d75fdf49099fb1dda933490e41a5f067a0b29184
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2025-04-24 11:40:23 +0300

    File system (src/fs) read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = path.join('files', 'fileToRead.txt');
+
+    try {
+        await fs.access(fileToRead);
+        console.log(`Файл ${fileToRead} найден.`);
+
+        const content = await fs.readFile(fileToRead, 'utf8');
+        console.log('Содержимое файла:');
+        console.log(content);
+    } catch (err) {
+        console.error('Ошибка:', err.message);
+        throw new Error('Операция с файловой системой не удалась');
+    }
 };
 
 await read();
