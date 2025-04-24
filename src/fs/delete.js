b3f58cdbf0b2f235d00ed651a8e0a8a11761a281
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2025-04-24 11:32:46 +0300

    File system (src/fs) delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const fileToDelete = path.join('files', 'fileToRemove.txt');
+
+    try {
+        // Проверка на существование файла
+        await fs.access(fileToDelete);
+        console.log(`Файл ${fileToDelete} найден.`);
+
+        // Удаление файла
+        await fs.unlink(fileToDelete);
+        console.log(`Файл ${fileToDelete} успешно удален.`);
+    } catch (err) {
+        console.error('Ошибка:', err.message);
+        throw new Error('Операция с файловой системой не удалась');
+    }
 };
 
 await remove();
