2c325c9290c11a3a0fae36333dcee60ff3405447
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2025-04-24 11:56:47 +0300

    Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = './files/fileToCalculateHashFor.txt';
+
+    if (!fs.existsSync(filePath)) {
+        throw new Error('Операция с файловой системой не удалась');
+    }
+
+    const hash = crypto.createHash('sha256'); // Создаем хэш с алгоритмом SHA256
+
+    const fileStream = fs.createReadStream(filePath);
+
+    // Чтение файла и вычисление хэша
+    fileStream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    // Завершаем вычисление хэша и выводим результат
+    fileStream.on('end', () => {
+        const hexHash = hash.digest('hex');
+        console.log(`SHA256 Hash: ${hexHash}`);
+    });
+
+    fileStream.on('error', (err) => {
+        console.error('Ошибка при чтении файла:', err);
+    }); 
 };
 
 await calculateHash();
