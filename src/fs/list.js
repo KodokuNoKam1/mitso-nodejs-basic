1e6bca51464fca7e5f772f0f223c9c33976b022c
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2025-04-24 11:35:58 +0300

    File system (src/fs) list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join('files');
+
+    try {
+        // Проверка на существование папки
+        await fs.access(folderPath);
+        console.log(`Папка ${folderPath} найдена.`);
+
+        // Получаем список файлов в папке
+        const files = await fs.readdir(folderPath);
+        console.log('Список файлов в папке:');
+        files.forEach(file => console.log(file));
+    } catch (err) {
+        console.error('Ошибка:', err.message);
+        throw new Error('Операция с файловой системой не удалась');
+    }
 };
 
 await list();
