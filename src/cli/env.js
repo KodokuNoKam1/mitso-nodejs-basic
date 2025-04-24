41995b25f2ecb7a3c6c5911496d3374c3c0035fd
KodokuNoKam1 <153433903+KodokuNoKam1@users.noreply.github.com>
2025-04-24 11:52:41 +0300

    Command line interface (src/cli)

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = Object.entries(process.env)
+        .filter(([key, value]) => key.startsWith('MITSO_')) // Фильтруем по префиксу MITSO_
+        .map(([key, value]) => `${key}=${value}`); // Форматируем как MITSO_name=value
+    console.log(envVars.join('; '));
 };
 
 parseEnv();
